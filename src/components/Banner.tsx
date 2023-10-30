a3c3efa9d24f50428be651814001328b38e01100
AshikRahmanDev <ashikrahman.dev@gmail.com>
2023-10-30 23:31:01 +0600

    banner design changed-2

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import banner from "../assets/banner/banner.png"
 
 export const Banner = () => {
   return (
-    <div className='xl:w-[1200px] w-[90%] mx-auto flex flex-col-reverse md:flex-row justify-center md:justify-between items-center min-h-[90vh]'>
+    <div className='xl:w-[1200px] w-[90%] mx-auto flex flex-col-reverse md:flex-row justify-center md:justify-between items-center h-[600px] md:min-h-[90vh]'>
         <div data-aos="fade-right" className='mt-5 md:mt-0'>
             <p className='text-primary hidden md:block text-2xl'>Welcome to</p>
             <h1 className='text-white hidden md:block text-4xl md:text-5xl lg:text-7xl'> Qu<span className='text-primary'>meex</span> – Your Partner in Web Development</h1>
