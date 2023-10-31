d22c9dac8002d0e3fef2e2740e78bb5a3452b1ee
AshikRahmanDev <ashikrahman.dev@gmail.com>
2023-10-31 13:56:07 +0600

    services card and content added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,12 +3,12 @@ import banner from "../assets/banner/banner.png"
 export const Banner = () => {
   return (
     <div className='xl:w-[1200px] w-[90%] mx-auto flex flex-col-reverse md:flex-row justify-center md:justify-between items-center h-[600px] md:min-h-[90vh]'>
-        <div data-aos="fade-right" className='mt-5 md:mt-0'>
+        <div data-aos="fade-right" data-aos-duration="1000" className='mt-5 md:mt-0'>
             <p className='text-primary hidden md:block text-2xl'>Welcome to</p>
             <h1 className='text-white hidden md:block text-4xl md:text-5xl lg:text-7xl'> Qu<span className='text-primary'>meex</span> – Your Partner in Web Development</h1>
             <p className=' md:my-8 text-white/70'>At Qumeex, we are dedicated to turning your digital dreams into reality. Our web development services are designed to empower your business with a strong online presence, ensuring you stand out in the ever-evolving digital landscape.</p>
         </div>
-        <div data-aos="fade-left" className='mt-5 md:mt-0'>
+        <div data-aos="fade-left" data-aos-duration="1000" className='mt-5 md:mt-0'>
             <div className="md:hidden">
               <p className='text-primary text-2xl'>Welcome to</p>
               <h1 className='text-white text-4xl md:text-5xl lg:text-7xl'> Qu<span className='text-primary'>meex</span> – Your Partner in Web Development</h1>
--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,4 +1,6 @@
 import { SectionTitle } from "../shared/SectionTitle"
+import {CgWebsite} from 'react-icons/cg'
+import {MdOutlineAddBusiness,MdSettingsApplications} from "react-icons/md"
 
 export const Service = () => {
   return (
@@ -10,14 +12,29 @@ export const Service = () => {
         />
         {/* services */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-6">
-            <div className="min-h-[280px] shadow-2xl border-white/5 border">
-
+            {/* service */}
+            <div data-aos="fade-right" data-aos-duration="1000" className="min-h-[280px] flex flex-col items-center justify-center shadow-2xl border-white/5 border rounded-md">
+              <CgWebsite className="text-primary text-7xl"/>
+              <div className="text-white text-center px-4 mt-7">
+                <h2 className="text-3xl font-bold">Custom Web Development</h2>
+                <p className="text-white/70">Whether you need a brand-new website or want to revamp an existing one, we're here to make it happen.</p>
+              </div>
             </div>
-            <div className="min-h-[280px] shadow-2xl border-white/5 border">
-
+            {/* service */}
+            <div data-aos="zoom-in" data-aos-duration="1000" className="min-h-[280px] flex flex-col items-center justify-center shadow-2xl border-white/5 border rounded-md">
+              <MdOutlineAddBusiness className="text-primary text-7xl"/>
+              <div className="text-white text-center px-4 mt-7">
+                <h2 className="text-3xl font-bold">E-Commerce Solutions</h2>
+                <p className="text-white/70">We build secure and scalable e-commerce websites, helping you take your products and services online.</p>
+              </div>
             </div>
-            <div className="min-h-[280px] shadow-2xl border-white/5 border">
-
+            {/* service */}
+            <div data-aos="fade-left" data-aos-duration="1000" className="min-h-[280px] flex flex-col items-center justify-center shadow-2xl border-white/5 border rounded-md">
+              <MdSettingsApplications className="text-primary text-7xl"/>
+              <div className="text-white text-center px-4 mt-7">
+                <h2 className="text-3xl font-bold">Web Application Development</h2>
+                <p className="text-white/70">From booking systems to social networks, we create powerful web applications tailored to your requirements.</p>
+              </div>
             </div>
         </div>
     </div>
